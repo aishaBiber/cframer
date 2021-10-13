0c43fc6e8be07ef46aa3baa1a316c66094a7f65b
biberDigi <53328177+biberDigi@users.noreply.github.com>
2021-10-13 10:35:31 -0700

    c

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import styles from "./Button.modules.css";
 
-export function Button({ title = "Title" }) {
+export function Button({ title = "Til" }) {
   return <button className={styles.button}>{title}</button>;
 }
